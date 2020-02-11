f25a7137971e43904ec056817a5dd76b655c1da4
Gary Oliver <go@ao-cs.com>
2020-02-11 11:31:49 -0800

    Ignore comments in manifest

--- loadall.sh
+++ loadall.sh
@@ -12,27 +12,29 @@ else
   MANIFEST="$1"
 fi
 
-for f in `cat $MANIFEST`; do
-  # See if target directories exist
-  if ! ampy -p $PORT put $f $f >/dev/null 2>&1; then
-    # An error, so create sub directories as needed
-    DIRS=`dirname $f | tr '/' ' '`
-    dir=''
-    if [ "$DIRS" != "." ]; then
-      for d in $DIRS; do
-        if [ -z "$dir" ]; then
-          dir=$d
-        else
-          dir=$dir/$d
-        fi
-        if ! ampy -p $PORT ls $dir >/dev/null 2>&1; then
-          echo Creating $dir
-          ampy -p $PORT mkdir $dir >/dev/null 2>&1
-        fi
-      done
+while read -r line; do
+  file=`echo $line`
+  if [[ "${file:0:1}" != "#" ]]; then
+    if ! ampy -p $PORT put $file $file >/dev/null 2>&1; then
+      # An error, so create sub directories as needed
+      DIRS=`dirname $file | tr '/' ' '`
+      dir=''
+      if [ "$DIRS" != "." ]; then
+        for d in $DIRS; do
+          if [ -z "$dir" ]; then
+            dir=$d
+          else
+            dir=$dir/$d
+          fi
+          if ! ampy -p $PORT ls $dir >/dev/null 2>&1; then
+            echo Creating $dir
+            ampy -p $PORT mkdir $dir >/dev/null 2>&1
+          fi
+        done
+      fi
+      # Do it again (report any errors)
+      ampy -p $PORT put $file $file
     fi
-    # Do it again (report any errors)
-    ampy -p $PORT put $f $f
+    echo Downloaded $file
   fi
-  echo Downloaded $f
-done
+done <$MANIFEST
